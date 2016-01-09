66d0207f8e7579ea2bc2ec246faa1ed2df734ce1
Aaron Foley <aaron.s.foley@gmail.com>
2016-01-10 03:33:40 +1100

    Imporved Search page

--- search.php
+++ search.php
@@ -9,12 +9,17 @@
 
 	<ul class="items">
 		<?php $i = 0; while(search_results()): $i++; ?>
-
+        <article>
 			<h1>
                 <a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
             </h1>
             <span class="post-date">Posted <?php echo article_date(); ?> by <?php echo article_author(); ?>.</span>
 
+            <div class="content">
+                <?php echo article_markdown(); ?>
+            </div>
+
+        </article>
 		<?php endwhile; ?>
 	</ul>
 
